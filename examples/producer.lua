2717fa51207100d1305b4887be92f864b84b5098
yuz10 <yuzhou@apache.org>
2023-03-03 21:08:01 +0800

    add batch example
    add batch example

--- examples/producer.lua
+++ examples/producer.lua
@@ -5,10 +5,12 @@ package.path = ';../lib/?.lua;' .. package.path
 local cjson = require("cjson")
 local producer = require "resty.rocketmq.producer"
 local admin = require "resty.rocketmq.admin"
+local utils = require("resty.rocketmq.utils")
+local split = utils.split
 
 local nameservers = { "127.0.0.1:9876" }
 
-local adm, err = admin.new({ "127.0.0.1:9876" })
+local adm, err = admin.new(nameservers)
 if not adm then
     print("new admin client err:", err)
     return
@@ -31,11 +33,20 @@ if not res then
     print("send err:", err)
     return
 end
-
 print("send success: " .. cjson.encode(res.sendResult))
 
+local res, err = p:batchSend {
+    { topic = "TopicTest", body = "batch1", },
+    { topic = "TopicTest", body = "batch2", }
+}
+if not res then
+    print("send err:", err)
+    return
+end
+print("send batch success: " .. cjson.encode(res.sendResult))
+
 -----------------viewMessage
-local msg, err = adm:viewMessage(res.sendResult.offsetMsgId)
+local msg, err = adm:viewMessage(split(res.sendResult.offsetMsgId, ',')[1])
 if not msg then
     print("viewMessage err:", err)
     return
